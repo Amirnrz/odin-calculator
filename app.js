44bf0e02d0ac748f25d97d952c00e92b1218d9f4
Amir Norozi <amirnrzdev002@gmail.com>
2023-12-25 13:02:01 +0330

    calculator design

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1,num2,operator) {
+  add()
+}
+
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
