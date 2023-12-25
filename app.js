681fbed9c0a8443c877ad5423229261166698536
Amir Norozi <amirnrzdev002@gmail.com>
2023-12-26 01:30:03 +0330

    calculator completed

--- app.js
+++ app.js
@@ -1,26 +1,95 @@
+const display = document.querySelector('.display')
+const clearBtn = document.querySelector('#clear-btn')
+const equalBtn = document.querySelector('#equal-btn')
+const digitBtns = document.querySelectorAll('[data-digit]')
+const operatorBtns = document.querySelectorAll('[data-op]')
+
+let firstNumber =''
+let secondNumber = ''
+let operator = ''
 
 
-let firstNumber;
-let secondNumber;
-let operator;
 
-function operate(num1,num2,operator) {
-  add()
+function operate(operator,num1,num2) {
+  num1 = parseFloat(num1)
+  num2 = parseFloat(num2)
+
+  switch (operator) {
+    case '+':
+      return add(num1,num2);
+    case '-':
+      return subtract(num1,num2);
+    case '*':
+      return multiply(num1,num2)
+    case '/':
+      return divide(num1,num2)
+  }
 }
 
 
-function add() {
-
+function add(a,b) {
+  return a + b
 }
 
-function subtract() {
-
+function subtract(a,b) {
+  return a - b
 }
 
-function multiply() {
-
+function multiply(a,b) {
+  return a * b
 }
 
-function divide() {
-
+function divide(a,b) {
+  if(b === 0) {
+    display.innerHTML = 'cannot divide to 0'
+    return
+  }
+  return a / b  
 }
+
+function clearCalculator() {
+  firstNumber = ''
+  secondNumber = ''
+  operator = ''
+  display.innerHTML = ''
+}
+
+function updateUI() {
+  display.innerHTML = firstNumber + ' ' + operator + ' ' + secondNumber
+}
+
+digitBtns.forEach(btn => {
+  btn.addEventListener('click',() => {
+    if(operator === '') {
+      firstNumber += btn.dataset.digit
+    } else {
+      secondNumber += btn.dataset.digit
+    }
+    updateUI()
+  })
+})
+
+
+operatorBtns.forEach(btn => btn.addEventListener('click',() => {
+  if(firstNumber !== '' && secondNumber !== '') {
+    const result = operate(operator,firstNumber,secondNumber)
+    firstNumber = result.toString()
+    secondNumber = ''
+  }
+  operator = btn.dataset.op
+  updateUI()
+}))
+
+clearBtn.addEventListener('click',clearCalculator)
+
+equalBtn.addEventListener('click',() => {
+  if(firstNumber !== '' && secondNumber !== '') {
+    const result = operate(operator,firstNumber,secondNumber)
+    if(result) {
+      firstNumber = result.toString()
+      operator = ''
+      secondNumber = ''
+      display.innerHTML = firstNumber
+    }
+  }
+})
